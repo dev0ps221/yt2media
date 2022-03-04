f2929b55d4d15ddfb6b2e3c7d5e7931510ab8ed4
dev0ps221 <seybaxscorpion@gmail.com>
2022-03-04 20:07:55 +0000

    webapp ... actions, rocks...., let's style a bit more

--- webcli/assets/js/master.js
+++ webcli/assets/js/master.js
@@ -1,4 +1,3 @@
-
 const socket = new TeeSioCliSocket()
 const viewtabs = document.querySelectorAll('nav ul li')
 
@@ -58,9 +57,77 @@ function doAction(){
 
 }
 
+function bytesToSize(bytes) {
+    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    if (bytes == 0) return '0 Byte';
+    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+    return Math.round(bytes / Math.pow(1024, i), 2) + ' ' + sizes[i];
+ }
+
+function renderVideoThumbnail(thumb){
+    return `
+        <span class='illu'>
+            <img class='illupic' src='${thumb.url}'>
+        </span>
+    `
+}
+
+function renderDownloadLinks(formats){
+    let rendered = ``
+    formats.forEach(
+        format=>{
+            rendered = `${rendered}
+            <li class='preview'>
+                <video src='${format.url}' controls/>
+            </li>
+            <li class='download'>
+                download ${format.ext} format
+                <ul>
+                    <li>
+                        Size : ${bytesToSize(format.filesize)}
+                    </li>
+                    <li>
+                        Download link : <a href='${format.url}'>${bytesToSize(format.filesize)}
+                    </li>
+                </ul>
+            </li>`
+        }
+    )
+    return rendered
+}
+
+function renderVidContent(video){
+    return `
+        <li class='videoresult'>
+            <ul>
+                <li class='title'>
+                    ${video.title}
+                </li>
+                ${  
+                    video.thumbnails && video.thumbnails.length ?
+                        `<li class='illus'>
+                            ${renderVideoThumbnail(video.thumbnails[0])}
+                        </li>`
+                    :   ''
+                }
+                <li class='downloads'>
+                    DOWNLOAD ${video.title}:
+                    <ul>
+                        ${renderDownloadLinks(video.requested_formats)}
+                    </ul>
+                </li>
+            </ul>
+        </li>
+    `
+}
+
 function refreshContentView(videos){
-    alert('lets refresh our content view')
-    console.log(videos)
+    const contentview = document.querySelector('#view ul')
+    videos.forEach(
+        video=>{
+            contentview.innerHTML+=renderVidContent(video)
+        }
+    )
 }
 
 function listenDoAction(){
